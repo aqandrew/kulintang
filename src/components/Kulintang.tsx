8edd8793455bd2d0543423d70129671770f240e9
Andrew Aquino <dawneraq@gmail.com>
2023-12-18 00:32:12 -0800

    play appropriate frequencies for each gong

--- src/components/Kulintang.tsx
+++ src/components/Kulintang.tsx
@@ -2,14 +2,30 @@ import { useEffect, useRef } from 'react';
 import useKeyboardBindings from '../hooks/useKeyboardBindings';
 import './Kulintang.css';
 
+type Note = {
+	name: string;
+	frequency: number;
+};
+
 // Pelog scale for gamelan
 // https://www.youtube.com/watch?si=F7sb2W6o500chwRT&t=114&v=FAi4RSDv4ig&feature=youtu.be
-const TONES = ['Bb', 'B', 'Db', 'E', 'F', 'Gb', 'Ab', 'Bb'];
+// frequencies from https://pages.mtu.edu/~suits/notefreqs.html
+const TONES: Note[] = [
+	{ name: 'Bb', frequency: 233.08 },
+	{ name: 'B', frequency: 246.94 },
+	{ name: 'Db', frequency: 277.18 },
+	{ name: 'E', frequency: 329.63 },
+	{ name: 'F', frequency: 349.23 },
+	{ name: 'Gb', frequency: 369.99 },
+	{ name: 'Ab', frequency: 415.3 },
+	{ name: 'Bb', frequency: 466.16 },
+];
 
 export default function Kulintang() {
 	const gongsRef = useRef<(HTMLElement | null)[]>([]);
-	let playSound: (tone: string, index: number) => void;
+	let playSound: (frequency: number) => void;
 
+	// trigger click events for keys 1-8
 	useKeyboardBindings(
 		TONES.reduce(
 			(keyMap, _, i) => ({
@@ -26,16 +42,17 @@ export default function Kulintang() {
 	useEffect(() => {
 		const audio = new AudioContext();
 		const gainNode = audio.createGain();
+		gainNode.gain.value = 0.3; // lower gain to prevent clipping
 		gainNode.connect(audio.destination);
 
-		playSound = function (tone: string, index: number) {
-			console.log(`playing ${tone}${index}`);
+		playSound = function (frequency: number) {
+			console.log(`playing ${frequency}`);
 
+			// TODO adjust ADSR instead of using 500ms timeout
 			const oscillator = audio.createOscillator();
 			oscillator.type = 'sine';
 			oscillator.connect(gainNode);
-			// TODO set frequency per tone
-			oscillator.frequency.value = 440;
+			oscillator.frequency.value = frequency;
 			oscillator.start();
 
 			setTimeout(() => {
@@ -50,15 +67,15 @@ export default function Kulintang() {
 
 	return (
 		<div className="Kulintang">
-			{TONES.map((tone, i) => (
+			{TONES.map(({ name, frequency }, i) => (
 				<button
 					ref={(element) => {
 						gongsRef.current[i] = element;
 					}}
-					onClick={() => playSound(tone, i)}
+					onClick={() => playSound(frequency)}
 					key={i}
 				>
-					{tone}
+					{name}
 				</button>
 			))}
 		</div>
