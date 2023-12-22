a5b5a8b9429ec85a2633214f6d1f596836b1a17b
Andrew Aquino <dawneraq@gmail.com>
2023-12-21 23:53:18 -0800

    use Tone.js for synths

--- src/components/Kulintang.tsx
+++ src/components/Kulintang.tsx
@@ -1,33 +1,26 @@
 import { useEffect, useRef } from 'react';
+import * as Tone from 'tone';
 import { TONES } from '../utils/notes';
 import useKeyboardBindings from '../hooks/useKeyboardBindings';
 import './Kulintang.css';
 
 export default function Kulintang() {
 	const gongsRef = useRef<(HTMLElement | null)[]>([]);
+	const synthRef = useRef<Tone.PolySynth>();
 	const playSoundRef = useRef<(frequency: number) => void>();
 
-	// audio logic wrapped in useEffect so that we can call audio.close() during cleanup/dismount
 	useEffect(() => {
-		const audio = new AudioContext();
-		const gainNode = audio.createGain();
-		gainNode.gain.value = 0.3; // lower gain to prevent clipping
-		gainNode.connect(audio.destination);
+		synthRef.current = new Tone.PolySynth().toDestination();
 
 		playSoundRef.current = function (frequency: number) {
 			console.log(`playing ${frequency}`);
 
 			// TODO adjust ADSR
-			const oscillator = audio.createOscillator();
-			oscillator.type = 'sine';
-			oscillator.connect(gainNode);
-			oscillator.frequency.value = frequency;
-			oscillator.start();
-			oscillator.stop(audio.currentTime + 0.5);
+			synthRef.current?.triggerAttackRelease(frequency, 0.25);
 		};
 
 		return () => {
-			audio.close();
+			synthRef.current?.dispose();
 		};
 	}, []);
 
